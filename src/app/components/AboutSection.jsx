297d2b7d9bbd8cb025c49fe70668267a28d0a99a
Daniel-Ruiz-Gtz <58959667+Daniel-Ruiz-Gtz@users.noreply.github.com>
2023-12-03 02:06:45 -0600

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -10,7 +10,14 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Python</li>
-        <li>Etc</li>
+        <li>Git</li>
+        <li>Shell</li>
+        <li>Linux</li>
+        <li>Scripting</li>
+        <li>Jenkins</li>
+        <li>Robot Framework</li>
+        <li>Testing Design</li>
+        <li>Agile Methologies</li>
       </ul>
     ),
   },
@@ -19,21 +26,55 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>
+        <li className="font-bold">
           Computer Science Engineering | Universidad de Guadalajara, Aug 2018 -
           Dec 2022
         </li>
-        <li> Automation Testing Intership | IBM, Jan 2022 - Jan 2023</li>
+        <p>
+          The computer engineering professional specializing in system software
+          will be able to design and develop foundational software systems (the
+          core programming systems on a computer); interact with digital and
+          telecommunications subsystems (networks); design and implement
+          operating systems; conceive and design new programming languages, as
+          well as build translators (compilers); specify computer architectures
+          and develop application software relevant to their field.
+        </p>
       </ul>
     ),
   },
   {
-    title: "Certifications",
-    id: "certifications",
+    title: "Profesional Experience",
+    id: "experience",
     content: (
       <ul className="list-disc pl-2">
-        <li>Text</li>
-        <li>Text</li>
+        <li className="font-bold">
+          Automation Testing Intership | IBM, Jan 2022 - Jan 2023
+        </li>
+        <p>
+          Internship mostly focused on test creation and process automation to
+          be able to stress some environments. During this time I have worked
+          with Python and Robotframework as main technologies. A very important
+          skill that I acquired in this period was the use of linux and knowing
+          how to move within the environment thanks to the use of commands and
+          shortcuts. I have worked with other secondary tools such as: Jenkins,
+          Jmeter in addition to the constant use of the terminal. The projects
+          in which I collaborated were: Hydra (TS7700) and iLAB (Sprectrum
+          Virtualize)
+        </p>
+        <li className="font-bold">
+          System Test Engineer | IBM, Jan 2022 - Actual
+        </li>
+        <p>
+          Technical Specialists in this job role will have expertise in the
+          planning, design, management, execution, and reporting of tests using
+          appropriate testing tools and techniques, and conforming to agreed
+          standards, to ensure that new and amended systems, together with any
+          interfaces, perform as specified together with the business. Typical
+          examples of the deliverables are testing strategies, test plans, test
+          cases, test reports, and quality metrics. Technical Specialists who
+          primarily apply their technical skills in an internal or external
+          customer billable services and implementation environment.
+        </p>
       </ul>
     ),
   },
@@ -56,10 +97,10 @@ const AboutSection = () => {
         <div>
           <h2 className="text-3xl font-bold mb-4"> About Me </h2>
           <p className="text-base lg:text-lg">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa hic
-            eaque consequatur quas qui, nam odio cum quod eveniet ullam
-            blanditiis saepe molestias possimus, alias voluptate? Reiciendis
-            assumenda excepturi accusamus.
+            I am a passionate Computer Engineering graduate from Universidad de
+            Guadalajara, Mexico, with a strong focus on Automation and
+            Scripting. I thoroughly enjoy developing solutions to automate tasks
+            and streamline processes, making life easier through code.
           </p>
           <div className="flex flex-row mt-8">
             <TabButton
@@ -77,11 +118,11 @@ const AboutSection = () => {
               Education{" "}
             </TabButton>
             <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
+              selectTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
             >
               {" "}
-              Certifications{" "}
+              Profesional Experience{" "}
             </TabButton>
           </div>
           <div className="mt-8">
