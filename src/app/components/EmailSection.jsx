167031a4bc8e082444cbfa224114695dd7f1ab3d
Daniel-Ruiz-Gtz <58959667+Daniel-Ruiz-Gtz@users.noreply.github.com>
2023-12-03 02:14:34 -0600

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -47,15 +47,17 @@ const EmailSection = () => {
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
+          My inbox is always open. Whether you have a question or just want to
+          say hi, I&apos;ll try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/Daniel-Ruiz-Gtz" target="_blank">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link
+            href="https://www.linkedin.com/in/danielruizgtz/"
+            target="_blank"
+          >
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
